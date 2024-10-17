238ac82763fb778cb951c6d7cb4c736164814f25
jrdnlowry <tatelwry@gmail.com>
2024-10-17 17:34:27 -0400

    Clean & encode data

--- svm_employee_attrition.R
+++ svm_employee_attrition.R
@@ -1,25 +1,71 @@
 rm(list=ls())
 
 set.seed(10)
-install.packages("ggcorrplot")
+
+install.packages("caret")
+install.packages("dplyr")
+
 #library(ggcorrplot)
 library(e1071)
 library(dplyr)
 library(tidyr)
+library(caret)
+
+
+
 help(svm)
 
+
+################################################################################
+# Read in and transform the data
+################################################################################
 readData = function(path){
   data = read.csv(path, stringsAsFactors = TRUE)
 
   # attempt to convert yes/no to numeric values
-  for (i in 1:length(data$Attrition)){
+  for (i in 1:nrow(data)){
+    # ordinal encoding with business travel
+    if (data$BusinessTravel[i] == "Non-Travel"){
+      data$BusTravel[i] = 1
+    } else if(data$BusinessTravel[i] == "Travel_Rarely"){
+      data$BusTravel[i] = 2
+    } else {
+      data$BusTravel[i] = 3 # indicates travel frequently
+    }
+    
+    # encode gender: M or F
+    if (data$Gender[i] == "Female"){
+      data$GenderVal[i] = 1
+    } else {
+      data$GenderVal[i] = 2
+    }
+    # encode Department
+    # Sales, R&D, & HR
+    if (data$Department[i] == "Sales"){
+      data$DepartmentVal[i] = 1
+    } else if (data$DepartmentVal[i] == "Research & Development"){
+      data$DepartmentVal[i] = 2
+    } else {
+      data$DepartmentVal[i] = 3
+    }
     #print(data$Attrition[i])
     if (data$Attrition[i] == "Yes"){
       data$AttrValue[i] = 1 
     } else {
       data$AttrValue[i] = 0
-    }
+    }    
+    
   }
+  
+  # drop uneeded columns
+  drops = c("StandardHours", "Over18", "EmployeeID",
+            "BusinessTravel", "Department", "EducationField",
+            "Gender", "JobRole", "MaritalStatus", "EmployeeCount")
+  data = data[,!(names(data) %in% drops)] 
+  # replace NAs with  a median value (numeric values only)
+  data = data %>% mutate(across(where(is.numeric), ~replace_na(., median(., na.rm=TRUE))))
+  
+  
 #  # test scale data to quadratic term
 #  for (i in 1:length(data$Age)){
 #    #print(data$Attrition[i])
@@ -32,31 +78,69 @@ readData = function(path){
 
 df = readData("C:/Users/tatel/OneDrive/Documents/Rstudio/datasets/attrition.csv")
 
-# drop not needed columns
-drops = c("StandardHours", "Over18", "EmployeeID",
-          "BusinessTravel", "Department", "EducationField",
-          "Gender", "JobRole", "MaritalStatus", "EmployeeCount")
-df = df[,!(names(df) %in% drops)]
-
 head(df)
-# replace NAs with median value for each column
-df %>% mutate(across(where(is.numeric), ~replace_na(., median(., na.rm=TRUE))))
 
-# SPLITTING DATA INTO TRAINING & TESTING
-# select 80% of data for training
-sample = sample.int(n = nrow(df), size = floor(0.80*nrow(df)), replace = FALSE)
-train = df[sample, ]
-# 20% of data for testing
-test = df[-sample, ]
-test = test[3:20]
+################################################################################
+# partition data out
+# 80% training; 10% testing
+################################################################################
 
-head(train)
-# value we are predicting 
-y = train$Attrition
-x = train[3:20]
+help(sample)
 
-# build the initial model with the training data
-svmfit = svm(x, y)
+split = function(d){
+  df = d
+  # shuffle the data: sample without replacement
+  dataSplit = sample(nrow(df), round(nrow(df)*.80))
+  # partition data into training and test
+  training = df[dataSplit, ]
+  testing = df[-dataSplit,]
+  #remaining_data = df[-dataSplit, ]
+  # split the remaining data 50/50: 50 val and 50 testing
+  #dfSplit = sample(nrow(remaining_data), round(nrow(remaining_data)*.50))
+  #testing = remaining_data[dfSplit,]
+  #val = remaining_data[-dfSplit,]
+  
+  dataList = list("training" = training,
+                  "testing" = testing)
+}
 
-print(svmfit)
-summary(svmfit)
+df = split(df)
+# we have our training and testing dataset contained in the df variable
+names(df)
+# sanity check
+nrow(df$training)
+nrow(df$testing)
+
+
+x = as.matrix(df$training[,-23])
+y = as.factor(df$training[,23])
+
+
+dat = data.frame(x = x, y = y)
+
+dat$x.MonthlyIncome = as.numeric(dat$x.MonthlyIncome)
+
+
+svmfit = svm(y~., data=dat,
+             kernel="linear", cost=10, scale=TRUE)
+
+names(svmfit)
+head(dat)
+
+names(dat$y)
+
+help(tune)
+kernels = c("linear", "radial")
+# creates a list of coeffiecents to test
+tune.out = tune(svm, y ~ ., data = dat, kernel = "radial",
+                ranges = list(cost = c(0.001, 0.01, 0.1, 1, 5, 10, 1000)))
+
+# access the cross-validation errors for each of these models
+summary(tune.out)
+
+names(tune.out)
+tune.out$best.parameters
+
+
+
+
